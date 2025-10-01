3e95fb219c01e78bc53fd1e786fdf240f97f7f5a
marcoalm32 <marcoalm25@gmail.com>
2025-10-01 10:19:47 -0300

    created database file

---
+++ db/database.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.DB_PORT;
+const HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME
+
+const mongouri = `mongodb://${HOST}:${PORT}/${DB_NAME}`;
+
+export const connectDatabase = () => {
+    console.log('Connecting to database...');
+    mongoose.connect(mongouri)
+    .then(() => {
+        console.log(`${DB_NAME} database connected successfully`);
+    })
+    .catch((error) => {
+        console.error('Database connection error:', error);
+        process.exit(1);
+    });
+}
